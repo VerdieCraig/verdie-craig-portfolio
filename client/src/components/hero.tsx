86669172d6570ed28b163fc5ac3194952d4ea836
VerdieCraig <craigverdie@gmail.com>
2025-12-15 11:24:20 -0500

    remove bg image from hero sxn

--- client/src/components/hero.tsx
+++ client/src/components/hero.tsx
@@ -15,10 +15,7 @@ export default function Hero() {
       <div className="absolute inset-0 bg-gradient-to-br from-primary/10 via-accent/5 to-secondary/10"></div>
       <div 
         className="absolute inset-0 bg-cover bg-center opacity-5"
-        style={{
-          backgroundImage: "url('https://images.unsplash.com/photo-1504639725590-34d0984388bd?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1974&h=1316')"
-        }}
-      ></div>
+        ></div>
       
       <div className="container mx-auto px-4 relative z-10">
         <div className="max-w-4xl mx-auto text-center">
